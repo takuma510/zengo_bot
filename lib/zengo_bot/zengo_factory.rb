1b85e41a0995f0a463bc444084dbe830d0f75069
Takuma Goto <takuma510@gmail.com>
2016-03-27 21:25:34 +0900

    non month list

--- lib/zengo_bot/zengo_factory.rb
+++ lib/zengo_bot/zengo_factory.rb
@@ -10,11 +10,21 @@ module ZengoBot
 
     # @return [Zengo]
     def today_one
-      zengo_list = @client.find_by_month Time.now.month.to_s
+      zengo_list = this_month_list + non_month_list
       select_one zengo_list
     end
 
     private
+    # @return [Array]
+    def this_month_list
+      @client.find_by_month Time.now.month.to_s
+    end
+
+    # @return [Array]
+    def non_month_list
+      @client.find_by_month '0'
+    end
+
     # @param zengo_list [Array]
     # @return [Zengo]
     def select_one(zengo_list)
